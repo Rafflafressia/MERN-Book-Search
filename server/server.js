2af2746eb79200f0f26c2c173ba6c5f9056da757
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-04-24 19:50:40 -0400

    Added resolvers

--- server/server.js
+++ server/server.js
@@ -3,19 +3,52 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Import ApolloServer class
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express');
+
+// Import the two parts of GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+const auth = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// ApolloServer instance
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    const user = auth.authMiddleware({req}).user;
+    return {user};
+  }
 });
+
+
+const startApolloServer = async () => {
+
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server));
+  
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  }
+  );
+  
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+
+};
+
+startApolloServer();
